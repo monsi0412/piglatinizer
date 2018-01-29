a5cc5858823c5f1bd0dc27ffaa6a90663f85136c
Monserrat <monserratdiaz12@gmail.com>
2018-01-29 23:21:24 +0000

    done

--- js/script.js
+++ js/script.js
@@ -10,53 +10,89 @@
 $(document).ready(function() {
 		
 	function setenceToPigLatin(){
+		// var vowels = ["a", "e", "i", "o", "u"]
 		var sentence = $("#userInput").val();
+		sentence = sentence.toString().toLowerCase();
 		var wordsArray = sentence.split(" ");
 		for(var i = 0; i < wordsArray.length; i++){
 			var char = wordsArray[i][0];
+			var secondChar = wordsArray[i][1];
+			var pigLatinized;
 			if(char==="a" || char === "e" || char === "i" || char === "o" || char === "u"){
-				var pigLatinized = wordsArray[i] + "yay";
+				pigLatinized = wordsArray[i] + "yay";
+			}
+			else if(char==="s" && secondChar==="h"){
+				var finalWord =wordsArray[i].substr(2);
+				pigLatinized = finalWord+char+secondChar+"ay";
 			}
 			else{
-				var finalWord =wordsArray[i].substr(1);
+				finalWord =wordsArray[i].substr(1);
 				pigLatinized = finalWord+char+"ay";
 			}
 			$("#result").append(pigLatinized + " ");
 		}
+	}
+	
+	
+	function sentenceToEnglish (){
+		var sentence = $("#userInputTwo").val();
+		var sentenceWords = sentence.split(" ");
 		
+		for(var i = 0; i<sentenceWords.length;i++){
+			var l = (sentenceWords[i].length);
+			var n = l -3;
+			var lastLetter = sentenceWords[i][n];
+			
+			if (sentenceWords[i][l-1] !=="y" && sentenceWords[i][l-2] !== "a"){
+				alert("Invalid Input, try again");
+                $("input[type=text], textarea").val("");
+                return null;
+			}
+			
+			
+			if (lastLetter==="y"){
+				newWord = sentenceWords[i].slice(0,-3);
+				var Englished = newWord;
+			}
+			else if(lastLetter !== "a" || lastLetter !== "e" || lastLetter !== "i" || lastLetter !== "o" || lastLetter !== "u"){
+				var newWord = sentenceWords[i].slice(0,-3);
+				
+				var Englished = lastLetter + newWord;
+			}
+			
+			$("#resultTwo").append(Englished + " ");
+		}
 	}
 	
 	$("#userInput").keyup(function(event) {
         if (event.keyCode === 13) {
-            $("button").click();
+            $("#translate").click();
         }
     });
 	
-	$("button").click(function(){
-		// var endResult = setenceToPigLatin();
-		// $("#result").html(endResult);
+	$("#translate").click(function(){
+		$("#result").html("");
 		setenceToPigLatin();
+	});
+	
+	$("#restart").click(function() {
+	   $("#result").html("");
+	   
+	   $("#userInput").val("");
+	});
+	
+	$("#userInputTwo").keyup(function(event) {
+        if (event.keyCode === 13) {
+            $("#untranslate").click();
+        }
+    });
+
+	$("#untranslate").click(function() {
+	   sentenceToEnglish(); 
 	});
 
 
 
-
-
-// Create the wordToPigLatin function that takes a word as a parameter and returns a transfromed word. 
-	// function wordToPigLatin(word){
-	// 	var first = str.substr(0);
-	// 	if(first === "a" || first === 'e' || first === 'i' || first === 'o' || first === 'u'){
-	// 		return word + "yay";
-	// 	}}
-
-
-
-
-// Create the sentenceToPigLatin function that takes a sentence as a parameter
-	//Loops through all the words in the sentence and transforms each word
-	//It should return a transfromed sentance
-
-	
 
 
 });
